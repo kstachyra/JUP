ac7b72941ba2cba5e59256dd654869c6f8f65f85
kstachyra <kacper.stachyra@gmail.com>
2017-01-25 23:27:17 +0100

    disconnect priority

--- src/jup/ftpModel/FtpDisconnectEvent.java
+++ src/jup/ftpModel/FtpDisconnectEvent.java
@@ -1,22 +1,5 @@
 package jup.ftpModel;
 
-import java.util.Objects;
-
 public class FtpDisconnectEvent extends FtpEvent
 {
-	@Override
-    public boolean equals(Object o)
-	{
-        if (!(o instanceof FtpDisconnectEvent))
-        	{
-        	System.out.println("DSFGDSFGFDSFG");return false;
-        	}
-        else return true;
-    }
-	
-	@Override
-    public int hashCode()
-	{
-        return Objects.hash(this.getClass());
-    }
 }
